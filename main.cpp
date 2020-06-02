9a6274122317b438b17cbf16e5cc8abdef546817
Shahriar201 <shahriarbrine201@gmail.com>
2020-06-02 23:30:17 +0600

    task on first class

---
+++ main.cpp
@@ -0,0 +1,108 @@
+#include<windows.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+void display(void)
+{
+/* clear all pixels */
+glClear (GL_COLOR_BUFFER_BIT);
+/* draw white polygon (rectangle) with corners at
+* (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
+*/
+glColor3f (0.0, 0.0, 0.0);
+
+
+    /*
+	glBegin(GL_QUADS); //Begin quadrilateral coordinates
+
+	//Trapezoid
+	glVertex3f(0.05f, 0.05f, -5.0f);
+	glVertex3f(0.35f, 0.05f, -5.0f);
+	glVertex3f(0.40f, 0.35f, -5.0f);
+	glVertex3f(0.00f, 0.35f, -5.0f);
+
+	glEnd(); //End quadrilateral coordinates
+	*/
+
+	glBegin(GL_TRIANGLES); //Begin triangle coordinates
+
+	//Triangle
+	glColor3f (1.0, 1.0, 1.0);
+	glVertex3f(0.06f, 0.06f, -5.0f);
+	glVertex3f(0.80f, 0.06f, -5.0f);
+	glVertex3f(0.06f, 0.40f, -5.0f);
+
+	glVertex3f(0.80f, 0.06f, -5.0f);
+	glVertex3f(0.80f, 0.40f, -5.0f);
+	glVertex3f(0.06f, 0.40f, -5.0f);
+
+    glColor3f (0.0, 1.0, 0.0);
+	glVertex3f(0.06f, 0.40f, -5.0f);
+	glVertex3f(0.80f, 0.40f, -5.0f);
+	glVertex3f(0.43f, 0.70f, -5.0f);
+
+	//Triangle
+	/*
+	glVertex3f(0.30f, 0.65f, -5.0f);
+	glVertex3f(0.60f, 0.65f, -5.0f);
+	glVertex3f(0.45, 0.85f, -5.0f);
+	*/
+
+	glEnd();//End triangle coordinates
+/* don't wait!
+* start processing buffered OpenGL routines
+*/
+
+
+	glBegin(GL_QUADS); //Begin quadrilateral coordinates
+
+	//Trapezoid
+	glColor3f (1.0, 0.0, 0.0);
+	glVertex3f(0.10f, 0.20f, -5.0f);
+	glVertex3f(0.30f, 0.20f, -5.0f);
+	glVertex3f(0.30f, 0.30f, -5.0f);
+	glVertex3f(0.10f, 0.30f, -5.0f);
+
+	glColor3f (0.25, 0.1, 0.75);
+	glVertex3f(0.35f, 0.00f, -5.0f);
+	glVertex3f(0.48f, 0.00f, -5.0f);
+	glVertex3f(0.48f, 0.33f, -5.0f);
+	glVertex3f(0.35f, 0.33f, -5.0f);
+
+	glColor3f (1.0, 0.0, 0.0);
+	glVertex3f(0.55f, 0.20f, -5.0f);
+	glVertex3f(0.75f, 0.20f, -5.0f);
+	glVertex3f(0.75f, 0.30f, -5.0f);
+	glVertex3f(0.55f, 0.30f, -5.0f);
+
+	glEnd(); //End quadrilateral coordinates
+
+glFlush ();
+}
+void init (void)
+{
+/* select clearing (background) color */
+glClearColor (0.0, 0.0, 0.0, 0.0);
+/* initialize viewing values */
+glMatrixMode(GL_PROJECTION);
+glLoadIdentity();
+glOrtho(0.0, 1.0, 0.0, 1.0, -10.0, 10.0);
+}
+/*
+* Declare initial window size, position, and display mode
+* (single buffer and RGBA). Open window with "hello"
+* in its title bar. Call initialization routines.
+* Register callback function to display graphics.
+* Enter main loop and process events.
+*/
+int main(int argc, char** argv)
+{
+glutInit(&argc, argv);
+glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+glutInitWindowSize (500, 500);
+glutInitWindowPosition (100, 100);
+glutCreateWindow ("hello");
+init ();
+glutDisplayFunc(display);
+glutMainLoop();
+return 0; /* ISO C requires main to return int. */
+}
